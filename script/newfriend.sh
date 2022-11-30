3330b757238699498c296b8a8eaa995b3cf3c2db
Thomas Vanhaudenard <118013478+Thomas-Vanh@users.noreply.github.com>
2022-11-30 16:17:08 +0100

    done

--- script/newfriend.sh
+++ script/newfriend.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
-echo " Hi master what can i do  for you? ask me anything date, joke, calculate. ctrl^C to quit me " 
-while true
+ 
+#non-interactive part 
+arg=$@
+
+if [[ -n $arg && $arg == *joke* ]];then #check if arg is not empty and if it match with joke
+	shuf -n 1 /home/thomas/devops/Bash_Scripting/BRIEFING-Bash_Scripting/script/joke.txt #shuffle the content of joke.txt 
+	exit 
+elif [[ -n $arg && $arg == *date* ]];then #same as above but with date 
+	date #print the date and time 
+	exit 
+elif [[ -n $arg && "$arg" -eq "$arg" ]] 2>/dev/null;then #check if arg is an equation  
+	tt=$(echo $arg | bc)
+	echo $tt
+	exit
+#if the argument are empty continue to interactive part 
+fi
+
+
+#interactive part 
+
+echo " Hi master what can i do  for you? ask me anything date, joke, calculate and quit to exit me "
+
+while true	 # infinite loop 
 do
-read -p ">$" insert
+read -p $'\e[48;5;202m'$(pwd)$'\e[0m'" >$"  insert
 	case "$insert" in 
 		*joke* )
-		shuf -n 1 /home/thomas/devops/Bash_Scripting/BRIEFING-Bash_Scripting/script/joke.txt
+		shuf -n 1 /home/thomas/devops/Bash_Scripting/BRIEFING-Bash_Scripting/script/joke.txt	#shuffle the content of joke.txt 
 		;;
 		*date* )
-		date
+		date	#print the date and time 
 		;;
-		*calculate*)
+		*calculate* )
 		echo "tell me your equation to resolve:"
 		read prob
-		tot=$(echo $prob | bc)
-		echo $tot
+		tot=$(echo $prob | bc) #create a variable called tot  and echo the equation piped to a bc  to calculate
+		echo $tot 	#print the total of the equation
 		;;
-	esac 
+		quit)
+		echo " Bye bye master"
+		exit
+		;;
+		*)
+	 	eval $insert	#set ur string to be a cmd
+		;;
+	
+	esac	#end of case 
 done
--- script/test.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-read -p "Are u human: " rep
-if [[ $rep = "yo" ||  $rep = "yes" || $rep = "y" ]]; then
-	echo "hello human"
-elif [[ $rep == "no" || $rep == "n" ]]; then
-	echo "Hello not human"
-else
-	echo "fuck off"
-fi
